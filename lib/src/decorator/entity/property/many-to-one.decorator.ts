90186676fdbf20beca0b79b78857c29dde9e30c8
chen <wszgrcy@gmail.com>
2019-11-26 09:48:28 +0800

    修复类型提示的bug

--- lib/src/decorator/entity/property/many-to-one.decorator.ts
+++ lib/src/decorator/entity/property/many-to-one.decorator.ts
@@ -10,7 +10,7 @@ import { RelationType } from "../../../type/relation.type";
  * 多对一,主键,指定对方字段
  * 多对多,?
  */
-export function ManyToOne<S = any, I = any>(inverseFn: () => Type<I>, inverseValueFn: (type: I) => keyof I, options?: any) {
+export function ManyToOne<S = any, I = any>(inverseFn: () => Type<I>, inverseValueFn: (type: I) => I[keyof I], options?: any) {
   return (target: Object, propertyKey: string) => {
     let list: any[] = Reflect.getMetadata(RELATION_SYMBOL, target.constructor) || []
     list.push({
