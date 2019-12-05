dbb33ff0999cfc086726682a6f6f10986a02d36d
chen <wszgrcy@gmail.com>
2019-12-05 14:19:04 +0800

    变更装饰器的传入参数类型

--- lib/src/decorator/entity/property/many-to-one.decorator.ts
+++ lib/src/decorator/entity/property/many-to-one.decorator.ts
@@ -1,8 +1,8 @@
-import { Type } from "@angular/core";
-import 'reflect-metadata'
-import { ONETOONE_SYMBOL, RELATION_SYMBOL } from "../../../symbol/entity.symbol";
-import { CyiaHttpService } from "../../../http";
-import { RelationType } from "../../../type/relation.type";
+import { Type } from '@angular/core';
+import 'reflect-metadata';
+import { ONETOONE_SYMBOL, RELATION_SYMBOL } from '../../../symbol/entity.symbol';
+import { CyiaHttpService } from '../../../http';
+import { RelationType } from '../../../type/relation.type';
 /**
  * doc 装饰器应该是附加在要生成的字段
  * 一对一,主键
@@ -10,9 +10,9 @@ import { RelationType } from "../../../type/relation.type";
  * 多对一,主键,指定对方字段
  * 多对多,?
  */
-export function ManyToOne<S = any, I = any>(inverseFn: () => Type<I>, inverseValueFn: (type: I) => I[keyof I], options?: any) {
+export function ManyToOne<S = any, I = any>(inverseFn: () => Type<I>, inverseValueFn: (type: I) => I[keyof I] | any, options?: any) {
   return (target: Object, propertyKey: string) => {
-    let list: any[] = Reflect.getMetadata(RELATION_SYMBOL, target.constructor) || []
+    const list: any[] = Reflect.getMetadata(RELATION_SYMBOL, target.constructor) || [];
     list.push({
       name: RelationType.ManyToOne,
       inverseFn,
@@ -20,9 +20,9 @@ export function ManyToOne<S = any, I = any>(inverseFn: () => Type<I>, inverseVal
       options,
       target: target.constructor,
       propertyName: propertyKey
-    })
-    Reflect.defineMetadata(RELATION_SYMBOL, list, target.constructor)
-  }
+    });
+    Reflect.defineMetadata(RELATION_SYMBOL, list, target.constructor);
+  };
 
 }
 
--- lib/src/decorator/entity/property/one-to-many.decorator.ts
+++ lib/src/decorator/entity/property/one-to-many.decorator.ts
@@ -1,8 +1,8 @@
-import { Type } from "@angular/core";
-import 'reflect-metadata'
-import { ONETOONE_SYMBOL, RELATION_SYMBOL } from "../../../symbol/entity.symbol";
-import { CyiaHttpService } from "../../../http";
-import { RelationType } from "../../../type/relation.type";
+import { Type } from '@angular/core';
+import 'reflect-metadata';
+import { ONETOONE_SYMBOL, RELATION_SYMBOL } from '../../../symbol/entity.symbol';
+import { CyiaHttpService } from '../../../http';
+import { RelationType } from '../../../type/relation.type';
 /**
  * doc 装饰器应该是附加在要生成的字段
  * 一对一,主键
@@ -10,11 +10,11 @@ import { RelationType } from "../../../type/relation.type";
  * 多对一,主键,指定对方字段
  * 多对多,?
  */
-export function OneToMany<S = any, I = any>(inverseFn: () => Type<I>, inverseValueFn: (type: I) => I[keyof I], options?: any) {
+export function OneToMany<S = any, I = any>(inverseFn: () => Type<I>, inverseValueFn: (type: I) => I[keyof I] | any, options?: any) {
   return (target: Object, propertyKey: string) => {
     // console.log(Reflect.getMetadataKeys(target.constructor)
     // );
-    let list: any[] = Reflect.getMetadata(RELATION_SYMBOL, target.constructor) || []
+    const list: any[] = Reflect.getMetadata(RELATION_SYMBOL, target.constructor) || [];
     // console.log(list, target.constructor);
     // debugger
     list.push({
@@ -24,12 +24,12 @@ export function OneToMany<S = any, I = any>(inverseFn: () => Type<I>, inverseVal
       options,
       target: target.constructor,
       propertyName: propertyKey
-    })
+    });
     // console.log(list, target);
     // console.log('准备附加2');
-    Reflect.defineMetadata(RELATION_SYMBOL, list, target.constructor)
+    Reflect.defineMetadata(RELATION_SYMBOL, list, target.constructor);
     // console.log('结束附加2');
-  }
+  };
 
 }
 
