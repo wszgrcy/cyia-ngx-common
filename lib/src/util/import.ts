e108a476b5b6546926ba099933e7a3906b1d75a6
chen-yang <wszgrcy@gmail.com>
2021-01-20 16:04:06 +0800

    feat: 增加style import

---
+++ lib/src/util/import.ts
@@ -0,0 +1,60 @@
+const importGroup: { [name: string]: Promise<ImportResult> } = {};
+export function importStyle(path: string, options = { document }): Promise<ImportResult> {
+  return (
+    importGroup[path] ||
+    (importGroup[path] = new Promise((resolve) => {
+      const onSuccess = (item: ImportResult) => {
+        resolve(item);
+      };
+
+      const link = options.document.createElement('link');
+      link.type = 'text/css';
+      link.href = path;
+
+      link.onload = () =>
+        onSuccess({
+          result: true,
+          msg: '',
+        });
+
+      link.onerror = (error: {}) =>
+        onSuccess({
+          result: false,
+          msg: error,
+        });
+      options.document.head.appendChild(link);
+    }))
+  );
+}
+
+export function importScript(path: string, options = { document }): Promise<ImportResult> {
+  return (
+    importGroup[path] ||
+    (importGroup[path] = new Promise((resolve) => {
+      const onSuccess = (item: ImportResult) => {
+        resolve(item);
+      };
+
+      const script = options.document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = path;
+
+      script.onload = () =>
+        onSuccess({
+          result: true,
+          msg: '',
+        });
+
+      script.onerror = (error: {}) =>
+        onSuccess({
+          result: false,
+          msg: error,
+        });
+      options.document.head.appendChild(script);
+    }))
+  );
+}
+export interface ImportResult {
+  result: boolean;
+  msg: any;
+}
--- lib/src/util/index.ts
+++ lib/src/util/index.ts
@@ -1,5 +1,5 @@
 export * from './strongly-typed';
 export * from './throw-if';
 export * from './merge-options';
-export * from './import-script';
+export * from './import';
 export * from './path.util';
