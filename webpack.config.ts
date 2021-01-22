bf848e3472ad857ddc6cdba66095068af4e4e942
chen-yang <wszgrcy@gmail.com>
2021-01-22 10:46:06 +0800

    build: 移除monaco构建配置

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,19 +1,19 @@
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+// const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 export default (config, options, targetOptions) => {
   console.log('webpack');
-  config.plugins.push(new MonacoWebpackPlugin({ languages: ['typescript', 'javascript'] }));
+  // config.plugins.push(new MonacoWebpackPlugin({ languages: ['typescript', 'javascript'] }));
 
-  const loaders: any[] = config.module.rules.filter(
-    (rule) => rule.use && rule.use.find((it) => it.loader && it.loader.includes('@angular-devkit\\build-optimizer'))
-  );
-  console.log(loaders.length);
+  // const loaders: any[] = config.module.rules.filter(
+  //   (rule) => rule.use && rule.use.find((it) => it.loader && it.loader.includes('@angular-devkit\\build-optimizer'))
+  // );
+  // console.log(loaders.length);
   // console.log('需要修改的', loaders);
-  loaders.forEach((loader) => {
-    const originalTest = loader.test;
-    loader.test = (file) => {
-      const isMonaco = !!file.match('monaco-editor');
-      return !isMonaco && !!file.match(originalTest);
-    };
-  });
+  // loaders.forEach((loader) => {
+  //   const originalTest = loader.test;
+  //   loader.test = (file) => {
+  //     const isMonaco = !!file.match('monaco-editor');
+  //     return !isMonaco && !!file.match(originalTest);
+  //   };
+  // });
   return config;
 };
