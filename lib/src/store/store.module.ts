7ebb09e1465f17741de3e8afeb107dbb9037c94e
chen-yang <wszgrcy@gmail.com>
2021-05-28 16:31:09 +0800

    build: 11.1.0

--- lib/src/store/store.module.ts
+++ lib/src/store/store.module.ts
@@ -4,8 +4,15 @@ import { StoreModuleForRootOptions } from './store.types';
 import { Store } from '@ngrx/store';
 import { ROOT_TOKEN } from './store.token';
 import { StoreBase } from './store.base';
-
-@NgModule({})
+import { CommonModule } from '@angular/common';
+export function serviceToStoreFactory(...stores) {
+  const obj = {};
+  for (const store of stores) {
+    createReducer(store)(obj);
+  }
+  return obj;
+}
+@NgModule({ imports: [CommonModule], providers: [], exports: [], declarations: [] })
 export class CyiaStoreModule {
   constructor(store: Store, @Inject(ROOT_TOKEN) serviceList: typeof StoreBase[], injector: Injector) {
     serviceList.forEach((service) => {
@@ -24,11 +31,7 @@ export class CyiaStoreModule {
         },
         {
           provide: options.token,
-          useFactory: (...stores) => {
-            return stores.reduce((pre, service) => {
-              return createReducer(service)(pre);
-            }, {});
-          },
+          useFactory: serviceToStoreFactory,
           deps: [options.stores],
         },
       ],
