691e3e5fbeb837a48161e71afab795aa2c072827
chen-yang <wszgrcy@gmail.com>
2021-05-29 17:40:03 +0800

    11.1.2

--- lib/src/store/index.ts
+++ lib/src/store/index.ts
@@ -2,3 +2,5 @@ export * from './store.service';
 export * from './store.module';
 export * from './store.types';
 export * from './decorator';
+export * from './store.base';
+export * from './store.token';
